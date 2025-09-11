0a0f6d20372a8fdd011ea30b373b9e6d54aed421
FMarcell06 <forgoo.marcell0@gmail.com>
2025-09-11 10:54:01 +0200

    Connected to Backend

--- src/assets/utils.js
+++ src/assets/utils.js
@@ -1,3 +1,28 @@
 export const randNr=(min,max)=>{
     return Math.floor(Math.random()*(max-min+1))+min
 }
+
+import axios from "axios"
+
+const baseUrl="http://localhost:8000/"
+
+//osszes adat lekerese
+
+export const getTodos= async(setTodos)=>{
+    const resp=await axios.get(baseUrl+"todos")
+    setTodos(resp.data)
+}
+
+//todo torlese
+export const deleteTodo=async (id)=>{
+    await axios.delete(`${baseUrl}todos/${id}`)
+}
+//uj hozzaadasa
+export const addTodo=async(descr)=>{
+    await axios.post(baseUrl+"todos",{descr})
+}
+//kessze atiras
+export const setDone=async(id)=>{
+    await axios.patch(`${baseUrl}todos/${id}`)
+}
+//osszes torlese
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { todosData } from "../data";
+//import { todosData } from "../data";
 import { useState } from "react";
 import { Button, ListGroup, ListGroupItem } from "reactstrap";
 import { IoTrashOutline } from "react-icons/io5";
 import { FaCheck } from "react-icons/fa6";
 import { NewTodo } from "./NewTodo";
 import { TodoSummary } from "./TodoSummary";
+import { useEffect } from "react";
+import { addTodo, deleteTodo, getTodos, setDone } from "../assets/utils";
 
 export const Todo = () => {
-  const [todos, setTodos] = useState(todosData);
-  console.log("renderelodik a Todo");
-  
+  const [todos, setTodos] = useState(null);
+  console.log("renderelodik a Todo",todos);
+
+  useEffect(()=>{
+    getTodos(setTodos)
+  },[])
+
+/*  
   const handleDelete=(id)=>{
     console.log(id);
     setTodos(prev=>prev.filter(obj=>obj.id!=id))
@@ -35,13 +42,28 @@ export const Todo = () => {
     }
     setTodos(prev=>[...prev,newTodo])
   }
+*/
+  const handleDelete=async(id)=>{
+    await deleteTodo(id)//az adatbazisban megtortenik a modositas
+    getTodos(setTodos)
+  }
+  const handleAdd=async(descr)=>{ 
+    await addTodo(descr)//az adatbazisban megtortenik a modostias
+    getTodos(setTodos)
+  }
+  const handleDone=async(id)=>{
+    await setDone(id)
+    getTodos(setTodos)
+  }
+  const clearTodos=()=>{}
+
 
   return (
     <div style={{maxWidth:"600px", display:"flex",alignItems:"center",flexDirection:"column",margin:"auto"}}>
       <NewTodo handleAdd={handleAdd}/>
       <Button onClick={clearTodos}><IoTrashOutline style={{color:"red",fontSize:"2rem"}}/></Button>
       <ListGroup>
-        {todos.map((obj) => (
+        {todos && todos.map((obj) => (
           <ListGroupItem key={obj.id} className="d-flex gap-1 jusify-content-between align-items-center">
             <FaCheck style={{color:obj.done ? "green" : "gray", fontSize:"2rem"}} onClick={()=>handleDone(obj.id)}/>
             <div style={{textDecoration:obj.done ? "line-through": "none"}}>
@@ -51,7 +73,7 @@ export const Todo = () => {
           </ListGroupItem>
         ))}
       </ListGroup>
-      <TodoSummary todos={todos}/>
+      {/*<TodoSummary todos={todos}/>*/}
     </div>
   );
 };
